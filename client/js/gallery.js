9afbcdd413e51fb78fea939a600b3bd29f84fcd2
TTKHAI-PC\DELL <ttkhai93@gmail.com>
2017-09-05 13:43:33 +0700

    Revert "t"
    This reverts commit 72a6438e6211a8c40515123ff9f4c8c2525fb13c.

--- client/js/gallery.js
+++ client/js/gallery.js
@@ -6,7 +6,7 @@ angular.module('nibs.gallery', [])
         $stateProvider
 
             .state('app.gallery', {
-                url: "/gallery/:isActiveCamera",
+                url: "/gallery",
                 views: {
                     'menuContent' :{
                         templateUrl: "templates/gallery.html",
@@ -50,7 +50,6 @@ angular.module('nibs.gallery', [])
     //Controllers
     .controller('GalleryCtrl', function ($scope, $rootScope, $state, $stateParams, $window, $ionicPopup, Picture) {
         var isCameraReady = false
-        var isUpdateAvatar = false
         var videoWidth = 0
         var videoHeight = 0
         $scope.isDeleteMode = false
@@ -61,11 +60,6 @@ angular.module('nibs.gallery', [])
             });
         }
         getPictures()
-
-        if ($stateParams.isActiveCamera == 'true') {
-            isUpdateAvatar = true
-            activeCamera()
-        }
 
         // Show and hide image checkbox
         $scope.switchMode = function() {
@@ -96,9 +90,11 @@ angular.module('nibs.gallery', [])
             var imgCheckboxs = document.getElementsByClassName('imgCheckbox')
             for(let i of imgCheckboxs) {
                 if (i.checked) {
+                    console.log('true')
                     return true
                 }
             }
+            console.log('false')
             return false
         }
 
@@ -178,7 +174,7 @@ angular.module('nibs.gallery', [])
 
             var canvas = document.getElementById('canvas');
             var img = canvas.toDataURL('image/jpeg')
-            $state.go("app.preview", {img: img, isUpdateAvatar: isUpdateAvatar});
+            $state.go("app.preview", {img: img, isUpdateAvatar: false});
         };
 
         function deletePicture() {
