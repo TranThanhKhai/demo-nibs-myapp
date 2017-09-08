2685132c64dea06db34bd7e57db517e54a78f51f
TTKHAI-PC\DELL <ttkhai93@gmail.com>
2017-09-08 16:37:58 +0700

    t

--- server/pictures.js
+++ server/pictures.js
@@ -87,7 +87,9 @@ function getBySecureURL(req, res, next) {
 
 function uploadPictureToCloud(req, res, next) {
     var file = req.body.file;
-    cloudinary.uploader.upload(file, function(result) {
+    var publicId = req.body.publicId;
+    console.log('publicId node: ' + publicId);
+    cloudinary.uploader.upload(file, {public_id: publicId }, function(result) {
         return res.send(JSON.stringify(result))
     })
 }
